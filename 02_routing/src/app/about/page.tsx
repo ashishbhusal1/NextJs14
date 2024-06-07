488066b0e6f6870bdb4dd9bcca6cb6de28a48d0f
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-06-07 22:46:55 +0545

    seo and metadata

--- 02_routing/src/app/about/page.tsx
+++ 02_routing/src/app/about/page.tsx
@@ -1,3 +1,7 @@
-export default function About(){
-    return <h1>About Page</h1>
+export const metadata = {
+  title: "About Metadata",
+};
+
+export default function About() {
+  return <h1>About Page</h1>;
 }
