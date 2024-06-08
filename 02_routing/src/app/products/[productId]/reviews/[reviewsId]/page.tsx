799be1fd2ae8c1302226e303c8c697fe4a86c363
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-06-08 15:22:42 +0545

    error handling

--- 02_routing/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ 02_routing/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,4 +1,8 @@
 import { notFound } from "next/navigation";
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
 export default function ReviewDetail({
   params,
 }: {
@@ -7,6 +11,10 @@ export default function ReviewDetail({
     reviewsId: string;
   };
 }) {
+  const random=getRandomInt(2);
+  if(random===1){
+    throw new Error("Error loading review")
+  }
   if (parseInt(params.reviewsId) > 1000) {
     notFound();
   }
