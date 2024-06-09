4f5e9cefada3c2b22cabeb772f2002149b402424
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-06-09 22:36:02 +0545

    conditional routes

--- 02_routing/src/app/complex-dashboard/layout.tsx
+++ 02_routing/src/app/complex-dashboard/layout.tsx
@@ -3,14 +3,17 @@ export default function DashboardLayout({
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
-    <>
+  const isLogedIn = false;
+  return isLogedIn ? (
+    <div>
       <div>{children}</div>
       <div style={{ display: "flex" }}>
         <div style={{ display: "flex", flexDirection: "column" }}>
@@ -19,6 +22,8 @@ export default function DashboardLayout({
         </div>
         <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
-    </>
+    </div>
+  ) : (
+    login
   );
 }
