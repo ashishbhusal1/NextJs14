4db5dc734f935d2319e2f7d64eda39f79f2b9535
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-06-21 09:25:34 +0545

    dynamic rendering

--- 04_rendering-demo/src/app/about/page.tsx
+++ 04_rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
+import { cookies } from "next/headers";
+
 export default function AboutPage() {
-    console.log("About server component");
-  return <h1>AboutPage</h1>;
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
+  console.log("About server component");
+  return <h1>AboutPage {new Date().toLocaleTimeString()}</h1>;
 }
