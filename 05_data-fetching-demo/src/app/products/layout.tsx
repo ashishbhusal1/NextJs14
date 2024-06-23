6456e0598a03856cb7fe8beb7e676b268b9ab04e
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-06-23 18:11:04 +0545

    time based revalidation

--- 05_data-fetching-demo/src/app/products/layout.tsx
+++ 05_data-fetching-demo/src/app/products/layout.tsx
@@ -3,7 +3,11 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  const response = await fetch("http://localhost:3001/products");
+  const response = await fetch("http://localhost:3001/products", {
+    next: {
+      revalidate: 20,
+    },
+  });
   const products = await response.json();
   console.log({ products });
   return <>{children}</>;
